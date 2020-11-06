6ed32de59bf2d53edbd721ce331905434d3d103e
spartak41ru <khudyak3@gmail.com>
2020-11-07 03:30:36 +1200

    UpDate event form inputs

--- src/Components/addNewEventForm/addNewEventForm.js
+++ src/Components/addNewEventForm/addNewEventForm.js
@@ -5,14 +5,42 @@ function addNewEventForm() {
  return(
      <>
          <Container>
-             <input/>
-             <input/>
-             <input/>
-             <input/>
-             <input/>
-             <input/>
-             <input/>
-             <input/>
+             <input
+                 placeholder={"Введите Имя"}
+                 required
+             />
+             <input
+                 placeholder={"Введите Фамилию"}
+                 required
+             />
+             <input
+                 placeholder={"Введите Отчество"}
+             />
+             <input
+                 type="tel"
+                 pattern="\(\d\d\d\) ?\d\d\d-\d\d-\d\d"
+                 placeholder="+7 (###) ###-##-##"
+                 required
+             />
+             <input
+                 type={"email"}
+                 placeholder={"Введите адресс эл. почты"}
+                 required
+             />
+             <input //к какой организации принадлежит пользователь
+
+             />
+             <input
+                type={"password"}
+                placeholder={"Введите пароль"}
+                required
+             />
+             <input
+                 type={"password"}
+                 placeholder={"Введите пороль ещё раз"}
+                 required
+
+             />
 
          </Container>
      </>
